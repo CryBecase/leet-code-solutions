fa38d2f0d6f7d9f1980b4d11fd3211155268a29f
wangzhen <1046060018@qq.com>
2020-02-25 12:59:39 +0800

    add No.226 solution

---
+++ 226. 翻转二叉树/main.go
@@ -0,0 +1,23 @@
+package main
+
+// https://leetcode-cn.com/problems/invert-binary-tree/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+func main() {
+
+}

0bcd9dc66ad2e649467f94c0a0dc4e3045bdf32b
wangzhen <1046060018@qq.com>
2020-07-19 13:14:14 +0800

    add No.312 solution

--- LeetCode301-400/312. 戳气球/main.go
+++ LeetCode301-400/312. 戳气球/main.go
@@ -3,36 +3,42 @@ package main
 // https://leetcode-cn.com/problems/burst-balloons/
 
 func maxCoins(nums []int) int {
-	// dp[i][j] 表示从第i+1个气球到第j+1个气球能获取的最大硬币数
+	if len(nums) == 0 {
+		return 0
+	}
+	// dp[i][j] 表示戳破从第i+1个气球到第j+1个气球能获取的最大硬币数
 	dp := make([][]int, len(nums))
 	for i := range dp {
 		dp[i] = make([]int, len(nums))
 	}
-	return helper(nums, dp, 0, len(nums)-1)
-}
+	for i := len(nums) - 1; i >= 0; i-- {
+		for j := i + 1; j < len(nums); j++ {
+			for k := i; k <= j; k++ {
+				// 选择第k+1个气球留到最后戳破
 
-func helper(nums []int, dp [][]int, i, j int) int {
-	if i > j {
-		return 0
-	}
-	if dp[i][j] > 0 {
-		return dp[i][j]
-	}
+				// 从第i+1个气球到第k个气球被戳破，从第k+2个气球到第j+1个气球被戳破时，戳破第k+1个气球时获得的硬币数
+				coinK := nums[k]
+				if i-1 >= 0 {
+					coinK *= nums[i-1]
+				}
+				if j+1 < len(nums) {
+					coinK *= nums[j+1]
+				}
 
-	for k := i; k <= j; k++ {
-		// 第k+1个气球在最后点爆
-		L := helper(nums, dp, i, k-1)
-		R := helper(nums, dp, k+1, j)
-		coins := nums[k]
-		if i-1 >= 0 {
-			coins *= nums[i-1]
+				// 当第k+1个气球留到最后被戳破的情况下，戳破第i+1到第j+1的所有气球获得的硬币数
+				coins := coinK
+				if k-1 >= 0 {
+					coins += dp[i][k-1]
+				}
+				if k+1 < len(nums) {
+					coins += dp[k+1][j]
+				}
+				dp[i][j] = maxFunc(dp[i][j], coins)
+			}
+
 		}
-		if j+1 < len(nums) {
-			coins *= nums[j+1]
-		}
-		dp[i][j] = maxFunc(dp[i][j], L+coins+R)
 	}
-	return dp[i][j]
+	return dp[0][len(nums)-1]
 }
 
 func maxFunc(i, j int) int {

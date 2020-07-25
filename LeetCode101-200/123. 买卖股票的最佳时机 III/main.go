421ec6546aebcced2a0af9e280961896fbb37570
wangzhen <1046060018@qq.com>
2020-07-25 14:21:04 +0800

    opt No.123 solution

--- LeetCode101-200/123. 买卖股票的最佳时机 III/main.go
+++ LeetCode101-200/123. 买卖股票的最佳时机 III/main.go
@@ -8,10 +8,8 @@ func maxProfit(prices []int) int {
 	if len(prices) == 0 {
 		return 0
 	}
-	// dp[i][1][0] 表示第i+1天买过1笔股票且手上没有股票的时候的最大利润
-	// dp[i][1][1] 表示第i+1天买过1笔股票且手上有股票的时候的最大利润
-	// dp[i][2][0] 表示第i+1天买过2笔股票且手上没有股票的时候的最大利润
-	// dp[i][2][1] 表示第i+1天买过2笔股票且手上有股票的时候的最大利润
+	// dp[i][k][0] 表示第i+1天允许k笔交易且手上没有股票的时候的最大利润
+	// dp[i][k][1] 表示第i+1天允许k笔交易且手上有股票的时候的最大利润
 	dp := make([][][]int, len(prices))
 	for i := range dp {
 		dp[i] = make([][]int, 3)
@@ -22,10 +20,10 @@ func maxProfit(prices []int) int {
 	dp[0][1][1] = -prices[0]
 	dp[0][2][1] = -prices[0]
 	for i := 1; i < len(prices); i++ {
-		dp[i][1][0] = maxFunc(dp[i-1][1][0], dp[i-1][1][1]+prices[i])
-		dp[i][1][1] = maxFunc(dp[i-1][1][1], -prices[i])
-		dp[i][2][0] = maxFunc(dp[i-1][2][0], dp[i-1][2][1]+prices[i])
-		dp[i][2][1] = maxFunc(dp[i-1][2][1], dp[i-1][1][0]-prices[i])
+		for k := 1; k <= 2; k++ {
+			dp[i][k][0] = maxFunc(dp[i-1][k][0], dp[i-1][k][1]+prices[i])
+			dp[i][k][1] = maxFunc(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i])
+		}
 	}
 	return dp[len(prices)-1][2][0]
 }

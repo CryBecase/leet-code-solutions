a2f389d99b04330f42bce6b0f9664518ea7cde83
wangzhen <1046060018@qq.com>
2020-09-05 11:07:30 +0800

    add No.46 Solution

--- LeetCode1-100/46. 全排列/main.go
+++ LeetCode1-100/46. 全排列/main.go
@@ -2,39 +2,35 @@ package main
 
 // https://leetcode-cn.com/problems/permutations/
 
-var result [][]int
-
 func permute(nums []int) [][]int {
-	result = make([][]int, 0, 2*len(nums))
+	result := make([][]int, 0)
+	used := make(map[int]bool)
+	path := make([]int, 0)
 
-	if len(nums) == 1 {
-		result = append(result, nums)
-		return result
+	dfs := func() {}
+	dfs = func() {
+		if len(path) == len(nums) {
+			tmp := make([]int, len(path))
+			copy(tmp, path)
+			result = append(result, tmp)
+			return
+		}
+
+		for _, num := range nums {
+			if used[num] {
+				continue
+			}
+
+			used[num] = true
+			path = append(path, num)
+			dfs()
+			path = path[:len(path)-1]
+			used[num] = false
+		}
 	}
-	visited := make([]int, len(nums), len(nums))
-
-	backtrack(nums, make([]int, 0), visited)
+	dfs()
 
 	return result
-}
-
-func backtrack(nums []int, tmp, visited []int) {
-	if len(tmp) == len(nums) {
-		r := make([]int, len(nums))
-		copy(r, tmp)
-		result = append(result, r)
-		return
-	}
-	for i := 0; i < len(nums); i++ {
-		if visited[i] == 1 {
-			continue
-		}
-		visited[i] = 1
-		tmp = append(tmp, nums[i])
-		backtrack(nums, tmp, visited)
-		visited[i] = 0
-		tmp = tmp[:len(tmp)-1]
-	}
 }
 
 func main() {

76c4aa2f45f1fd1dc931477ce79e52fa0d78e103
wangzhen <1046060018@qq.com>
2020-09-05 10:28:13 +0800

    add No.459 Solution

---
+++ LeetCode1-100/60. 第k个排列/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+// https://leetcode-cn.com/problems/permutation-sequence/
+
+// 0 ~ 9 的阶乘
+var factorials = []int{
+	1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880,
+}
+
+func getPermutation(n int, k int) string {
+	path := strings.Builder{}
+	used := make(map[int]bool)
+
+	dfs := func() {}
+	dfs = func() {
+		if path.Len() == n {
+			return
+		}
+
+		// 未来要进入 path 的数字的全排列的个数
+		cnt := factorials[n-path.Len()-1]
+
+		for i := 1; i <= n; i++ {
+			if used[i] {
+				continue
+			}
+
+			if cnt < k {
+				k -= cnt
+				continue
+			}
+
+			used[i] = true
+			path.WriteString(strconv.Itoa(i))
+			dfs()
+			return
+		}
+	}
+	dfs()
+
+	return path.String()
+}
+
+func main() {
+
+}

d3b781ac335182e3c40d29aaec57e6e21220960e
wangzhen <1046060018@qq.com>
2020-09-12 11:58:14 +0800

    opt No.637 solution

--- LeetCode601-700/637. 二叉树的层平均值/main.go
+++ LeetCode601-700/637. 二叉树的层平均值/main.go
@@ -1,21 +1,6 @@
 package main
 
-/**
-https://leetcode-cn.com/problems/average-of-levels-in-binary-tree
-给定一个非空二叉树, 返回一个由每层节点平均值组成的数组.
-
-示例 1:
-
-输入:
-    3
-   / \
-  9  20
-    /  \
-   15   7
-输出: [3, 14.5, 11]
-解释:
-第0层的平均值是 3,  第1层是 14.5, 第2层是 11. 因此返回 [3, 14.5, 11].
-*/
+// https://leetcode-cn.com/problems/average-of-levels-in-binary-tree
 
 type TreeNode struct {
 	Val   int
@@ -25,23 +10,22 @@ type TreeNode struct {
 
 func averageOfLevels(root *TreeNode) []float64 {
 	var result []float64
-	currentLayer := []*TreeNode{root}
+	queue := []*TreeNode{root}
 	var nextLayer []*TreeNode
 	var sum float64
-	for len(currentLayer) != 0 {
-		// 广度优先遍历 BFS
-		for _, v := range currentLayer {
+	for len(queue) != 0 {
+		for _, v := range queue {
 			if v.Left != nil {
 				nextLayer = append(nextLayer, v.Left)
 			}
 			if v.Right != nil {
 				nextLayer = append(nextLayer, v.Right)
 			}
-			sum = sum + float64(v.Val)
+			sum += float64(v.Val)
 		}
-		result = append(result, sum/float64(len(currentLayer)))
+		result = append(result, sum/float64(len(queue)))
 		sum = 0
-		currentLayer = nextLayer
+		queue = nextLayer
 		nextLayer = []*TreeNode{}
 	}
 	return result

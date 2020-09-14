823c545c179d3153d4b831481422c3e1363accad
wangzhen <1046060018@qq.com>
2020-09-14 22:15:00 +0800

    opt No.94 solution

--- LeetCode1-100/94. 二叉树的中序遍历/main.go
+++ LeetCode1-100/94. 二叉树的中序遍历/main.go
@@ -9,17 +9,39 @@ type TreeNode struct {
 }
 
 func inorderTraversal(root *TreeNode) []int {
-	stack := make([]*TreeNode, 0)
+	type coloredNode struct {
+		color int
+		node  *TreeNode
+	}
+
+	stack := make([]*coloredNode, 0)
+	stack = append(stack, &coloredNode{
+		color: 1,
+		node:  root,
+	})
 	result := make([]int, 0)
-	for root != nil || len(stack) > 0 {
-		for root != nil {
-			stack = append(stack, root)
-			root = root.Left
-		}
-		root = stack[len(stack)-1]
+	for len(stack) > 0 {
+		cn := stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
-		result = append(result, root.Val)
-		root = root.Right
+		if cn.node == nil {
+			continue
+		}
+		if cn.color == 0 {
+			result = append(result, cn.node.Val)
+		} else {
+			stack = append(stack, &coloredNode{
+				color: 1,
+				node:  cn.node.Right,
+			})
+			stack = append(stack, &coloredNode{
+				color: 0,
+				node:  cn.node,
+			})
+			stack = append(stack, &coloredNode{
+				color: 1,
+				node:  cn.node.Left,
+			})
+		}
 	}
 	return result
 }

4c97ac2e0a57ffc92ec51eafe321bc1edba6efc3
wangzhen <1046060018@qq.com>
2020-09-16 21:44:07 +0800

    opt NO.94 Solution

--- LeetCode1-100/94. 二叉树的中序遍历/main.go
+++ LeetCode1-100/94. 二叉树的中序遍历/main.go
@@ -10,38 +10,38 @@ type TreeNode struct {
 
 // 颜色标记法 模拟递归
 func inorderTraversal(root *TreeNode) []int {
-	type coloredNode struct {
+	type Node struct {
 		color int // color 为 1 表示进入递归，为 0 表示执行操作
 		node  *TreeNode
 	}
 
-	stack := make([]*coloredNode, 0)
+	stack := make([]*Node, 0)
 
-	stack = append(stack, &coloredNode{
+	stack = append(stack, &Node{
 		color: 1,
 		node:  root,
 	})
 	result := make([]int, 0)
 	for len(stack) > 0 {
-		cn := stack[len(stack)-1]
+		n := stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
-		if cn.node == nil {
+		if n.node == nil {
 			continue
 		}
-		if cn.color == 0 {
-			result = append(result, cn.node.Val)
+		if n.color == 0 {
+			result = append(result, n.node.Val)
 		} else {
-			stack = append(stack, &coloredNode{
+			stack = append(stack, &Node{
 				color: 1,
-				node:  cn.node.Right,
+				node:  n.node.Right,
 			})
-			stack = append(stack, &coloredNode{
+			stack = append(stack, &Node{
 				color: 0,
-				node:  cn.node,
+				node:  n.node,
 			})
-			stack = append(stack, &coloredNode{
+			stack = append(stack, &Node{
 				color: 1,
-				node:  cn.node.Left,
+				node:  n.node.Left,
 			})
 		}
 	}

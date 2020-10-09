58ab8db48540635dbae704a3b8613f016a2701e5
wangzhen <1046060018@qq.com>
2020-10-09 14:15:37 +0800

    opt No.141 Solution

--- LeetCode101-200/141. 环形链表/main.go
+++ LeetCode101-200/141. 环形链表/main.go
@@ -1,29 +1,6 @@
 package main
 
-/**
-https://leetcode-cn.com/problems/linked-list-cycle
-给定一个链表，判断链表中是否有环。
-
-为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
-
-示例 1：
-
-输入：head = [3,2,0,-4], pos = 1
-输出：true
-解释：链表中有一个环，其尾部连接到第二个节点。
-
-示例 2：
-
-输入：head = [1,2], pos = 0
-输出：true
-解释：链表中有一个环，其尾部连接到第一个节点。
-
-示例 3：
-
-输入：head = [1], pos = -1
-输出：false
-解释：链表中没有环。
-*/
+// https://leetcode-cn.com/problems/linked-list-cycle
 
 type ListNode struct {
 	Val  int
@@ -34,16 +11,18 @@ func hasCycle(head *ListNode) bool {
 	slow, fast := head, head
 	for fast != nil {
 		fast = fast.Next
-		if fast != nil {
-			fast = fast.Next
+		if fast == nil {
+			return false
 		}
-		slow = slow.Next
 		if slow == fast {
-			if slow == nil {
-				return false
-			}
 			return true
 		}
+		fast = fast.Next
+		if slow == fast {
+			return true
+		}
+
+		slow = slow.Next
 	}
 	return false
 }
